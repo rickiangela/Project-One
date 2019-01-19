71260ea1628252d9d72f596ab9ebe7bf89162f5e
Ryan <franciscogutierrez@Franciscos-MacBook-Pro.local>
2019-01-19 09:16:08 -0500

    updates from ryan

---
+++ assets/javascript/main.js
@@ -0,0 +1,84 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD2x5ejtvQv8l5fMdsenPXuxIMbiiWDay0",
+    authDomain: "project-one-34b56.firebaseapp.com",
+    databaseURL: "https://project-one-34b56.firebaseio.com",
+    projectId: "project-one-34b56",
+    storageBucket: "project-one-34b56.appspot.com",
+    messagingSenderId: "87245655562"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var parentName = "";
+var childName = "";
+var childAge = "";
+var displayName = "";
+
+$("#btn1").on("click", function (event) {
+
+    event.preventDefault();
+
+    var email = $("#InputParent1").val().trim()
+    var password = $("#InputChild1").val().trim()
+
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+    }).then(function(){
+        $(".done").text("")
+        window.location.href = "account.html"
+    });
+
+});
+
+$("#newUserbtn").on("click", function (e) {
+
+    event.preventDefault();
+    var parentName = $("#inputName").val().trim();
+    var childName = $("#inputChild").val().trim();
+    var childAge = $("#inputAge").val().trim();
+    var displayName = $("#displayName1").val().trim();
+    var email = $("#InputEmail1").val().trim();
+    var password = $("#InputPassword1").val().trim();
+
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+        }).then(function () {
+            firebase.auth().currentUser.updateProfile({
+            displayName: displayName
+        }).then(function () {
+            console.log("It Works!")
+        }).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+        }).then(function (userId) {
+            var userId = firebase.auth().currentUser;
+            console.log(userId)
+            firebase.database().ref("users/" + userId.uid).set({
+                parentName: parentName,
+                childName: childName,
+                childAge: childAge,
+                displayName: displayName,
+                email: email
+        }).then(function(){
+            $(".done").text("")
+            window.location.href = "account.html"
+            });
+        });
+    });
+});
+
--- assets/javascript/parent.js
+++ assets/javascript/parent.js
@@ -1,17 +1,53 @@
-$("#submitBtn").on("click",function (event){
+$("#searchEnter").on("click",function (event){
     
     event.preventDefault();
 
-    var search = $("#searchInput").val().trim();
-    var queryURL = "https://newsapi.org/v2/everything?q=" + search + "&apiKey=3d72ad8b736142c3a32863f235626190";
+    var search = $("#searchVal").val().trim();
+    var queryURL = "https://newsapi.org/v2/everything?q=" + search + "&apiKey=e399cee38ee9457b83fb4fa2819d53da";
    
     console.log(queryURL)
+
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
 
         console.log(response);
+
+        var results = response.articles;
+
+        for (var i=0; i< 4; i++) {
+            var author = results[i].author;
+            var description = results[i].description;
+            var fullarticle = results[i].url;
+            var title = results[i].title;
+
+            var mainDiv = $("<div>").addClass("card");
+
+            var bodyDiv = $("<div>").addClass("card-body");
+
+            var h5 = $("<h5>").text("Title: " + title);
+            h5.addClass("card-title");
+
+            var p = $("<p>").text(description);
+            p.addClass("card-text");
+
+            var authorDiv = $("<h6>").text("Author: " + author)
+
+            var a = $("<a>").attr("href",fullarticle)
+            a.addClass("btn btn-primary")
+            a.text("Read Full Article")
+
+            bodyDiv.append(h5)
+            bodyDiv.append(p)
+            bodyDiv.append(authorDiv)
+            bodyDiv.append(a)
+
+            mainDiv.append(bodyDiv)
+
+            $("#articles").prepend(mainDiv)
+
+        };
     });
     
 });
