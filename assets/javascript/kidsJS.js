a7b5d4be868e98aa8be140e1b9570a0418cd5763
gutierrez88 <44066358+gutierrez88@users.noreply.github.com>
2019-01-22 19:57:01 -0500

    Update kidsJS.js

--- assets/javascript/kidsJS.js
+++ assets/javascript/kidsJS.js
@@ -122,7 +122,10 @@ window.onload = function video() {
         restRequest2.execute(function (resp) {
             // console.log(resp);
             for (i in resp.items) {  
-                $(".container3").append('<iframe width="250" height="auto" src="//www.youtube.com/embed/' + resp.items[i].id.videoId + '" frameborder="0" allowfullscreen></iframe>');
+                div = ("<div>")
+                div.addClass("m-2")
+                div.html("<iframe width="250" height="auto" src="//www.youtube.com/embed/' + resp.items[i].id.videoId + '" frameborder="0" allowfullscreen></iframe>");
+                $(".container3").append(div);
             }
         });
     });
